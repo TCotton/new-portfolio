79d0f202873f94bbc98d3d6ad42c4a1acd1a0bba
Andy Walpole <me@andywalpole.me>
2019-04-02 13:31:21 +0100

    new css reset file

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,12 +5,13 @@
  * See: https://www.gatsbyjs.org/docs/static-query/
  */
 
-import React from "react"
-import PropTypes from "prop-types"
-import { StaticQuery, graphql } from "gatsby"
+import React from 'react'
+import PropTypes from 'prop-types'
+import { StaticQuery, graphql } from 'gatsby'
 
-import Header from "./header"
-import "./layout.css"
+import Header from './header'
+import 'css/reset.css'
+import './layout.css'
 
 const Layout = ({ children }) => (
   <StaticQuery
