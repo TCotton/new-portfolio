472756e2adad02bca1c64e2b08eaa0ad08746fd2
Andy Walpole <me@andywalpole.me>
2019-04-03 16:22:43 +0100

    fully working index class test

--- src/components/seo.js
+++ src/components/seo.js
@@ -98,6 +98,7 @@ const detailsQuery = graphql`
             siteMetadata {
                 title
                 author
+								description
             }
         }
     }
--- src/pages/index.js
+++ src/pages/index.js
@@ -28,6 +28,7 @@ const IndexPage = () => (
 		<div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
 			<Image />
 		</div>
+
 		<Link to="/page-2/">
 			{go}
 		</Link>
