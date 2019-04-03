ec989d8a8dd88527879504a0969eb102176abd01
Andy Walpole <me@andywalpole.me>
2019-04-03 13:41:22 +0100

    added full test for header component

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,29 @@
 module.exports = {
+	coverageThreshold: {
+		'global': {
+			'branches': 40,
+			'functions': 40,
+			'lines': 40,
+			'statements': 40
+		}
+	},
+	collectCoverageFrom: [
+		'**/*.{js,jsx}'
+	],
+	coveragePathIgnorePatterns: [
+		'gatsby-browser.js',
+		'gatsby-config.js',
+		'gatsby-node.js',
+		'gatsby-ssr.js',
+		'jest.config.js',
+		'coverage/*',
+		'public/*',
+		'jest-preprocess.js'
+	],
 	transform: {
 		"^.+\\.jsx?$": `<rootDir>/jest-preprocess.js`,
 	},
+	testMatch: [ '**/?(*.)+(spec).js?(x)' ],
 	moduleNameMapper: {
 		".+\\.(css|styl|less|sass|scss)$": `identity-obj-proxy`,
 		".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": `<rootDir>/__mocks__/file-mock.js`,
